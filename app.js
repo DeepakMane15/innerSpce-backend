a323925ba4b9e8a0da17fa499129f9b8a961745b
Deepak Mane <manedeep2001@gmail.com>
2023-07-11 19:00:24 +0530

    request body size extended

--- app.js
+++ app.js
@@ -35,10 +35,12 @@ app.set('view engine', 'jade');
 
 connectDB()
 app.use(cors());
-app.use(bodyParser())
+// app.use(bodyParser())
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
