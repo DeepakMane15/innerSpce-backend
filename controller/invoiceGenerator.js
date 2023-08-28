581bac50820a966532dfad62ff48e0b5696c66e7
DeepakFromZeus <manedeep2001@gmail.com>
2023-08-28 23:29:59 +0530

    invoice challan

---
+++ controller/invoiceGenerator.js
@@ -0,0 +1,68 @@
+
+const fs = require('fs');
+const puppeteer = require('puppeteer')
+const handlebars = require('handlebars');
+const path = require('path')
+
+const generateInvoice = async (req, res) => {
+
+    console.log("here");
+
+    const { name } = req.body;
+
+    const customerName = name || 'John Doe';
+
+    try {
+
+        const host = req.headers.host;
+        const protocol = req.headers['x-forwarded-proto'] || 'http';
+        // return `${protocol}://${host}/images/${relativePath}`;
+
+        const logo = "http://localhost:3100/images/Picture.png";
+        const tline = "http://localhost:3100/images/tline.png";
+        const kb = "http://localhost:3100/images/kb.png";
+
+        // read our invoice-template.html file using node fs module
+        const file = fs.readFileSync('./views/invoice-template.html', 'utf8');
+
+        // console.log(file);
+
+        // return;
+
+        const template = handlebars.compile(file);
+
+        const html = template({ customerName, logo, tline, kb });
+
+        // simulate a chrome browser with puppeteer and navigate to a new page
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        // set our compiled html template as the pages content
+        // then waitUntil the network is idle to make sure the content has been loaded
+        await page.setContent(html, { waitUntil: 'networkidle0' });
+
+        // convert the page to pdf with the .pdf() method
+        const pdf = await page.pdf({ format: 'A4' });
+        await browser.close();
+
+        // send the result to the client
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=invoice.pdf');
+        res.send(pdf);
+
+    }
+    catch (err) {
+        console.log(err)
+        res.send({ status: 400, message: err, process: "generateInvoice" })
+    }
+}
+
+
+function generateFullImagePath(req, imageName) {
+    const host = req.headers.host;
+    const protocol = req.headers['x-forwarded-proto'] || 'http';
+    return `${protocol}://${host}/images/${imageName}`;
+}
+
+module.exports = { generateInvoice };
--- routes/invoice.js
+++ routes/invoice.js
@@ -3,13 +3,17 @@ var router = express.Router();
 
 const { requireSignin } = require('../middleware');
 const { addProductMaster, getProductMaster } = require('../controller/productMaster');
-const { addTransaction, getTransaction,getTransactionById } = require('../controller/invoice');
+const { addTransaction, getTransaction, getTransactionById } = require('../controller/invoice');
+const { generateInvoice } = require('../controller/invoiceGenerator');
 
 
 /* GET users listing. */
 router.post('/', requireSignin, addTransaction);
 router.get('/get', requireSignin, getTransaction);
 router.get('/get/:id', requireSignin, getTransactionById);
+
+router.post('/download-invoice', requireSignin, generateInvoice);
+
 // router.get('/get/:categoryId/:subCategoryId/:productId', requireSignin, getTransaction);
 
 
